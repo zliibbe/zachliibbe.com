de864a51a44443c62b0c1266b830e635a0e0a46b
Zach Liibbe <zliibbe@gmail.com>
2023-04-25 11:15:46 -0600

    Update text on index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+          Currently a&nbsp;
+          <code className={styles.code}>Work In Progress...</code>
         </p>
         <div>
           <a
