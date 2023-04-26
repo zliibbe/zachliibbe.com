fe1c28c8aeb2f4284cca260f9063e9cbc40d9a57
Zach Liibbe <zliibbe@gmail.com>
2023-04-26 16:01:36 -0600

    Remove boilerplate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,43 +5,10 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Currently a&nbsp;
-          <code className={styles.code}>Work In Progress...</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/favicon.io"
-              alt="Zach Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}></div>
-
+      <h1>Heading in <code>src/app/page.tsx</code></h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore dolorem perferendis et eveniet rem expedita accusantium eius, voluptatum sequi blanditiis quibusdam beatae quasi enim, ullam ratione libero? Inventore, beatae amet.</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus sed dolore deleniti dolorum hic vero!</p>
+      <p>Cupcake ipsum dolor sit amet. Tootsie roll shortbread dragée tart fruitcake halvah chupa chups danish. Pie tart cake gummies lemon drops dragée chocolate cake powder. Cotton candy sweet dessert sweet roll jelly. Cake cupcake fruitcake sweet dessert wafer wafer pie. Pie jelly marzipan jelly sweet roll. Chocolate dragée sugar plum cake powder. Tootsie roll pudding ice cream chupa chups cotton candy pastry gummi bears muffin. Lollipop jelly beans tart tart chocolate bar donut pastry carrot cake soufflé. Jelly beans biscuit liquorice cake croissant wafer cotton candy pastry croissant. Candy canes cupcake cookie danish jelly beans wafer fruitcake. Tart gummies dessert cotton candy topping oat cake. Sweet roll fruitcake icing jelly marshmallow cookie dessert bonbon.</p>
     </main>
   )
 }
