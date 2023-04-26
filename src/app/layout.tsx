2603ead8ca7e326dd6413f917a53935218f2ca49
Zach Liibbe <zliibbe@gmail.com>
2023-04-26 16:01:18 -0600

    Update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Zach Liibbe',
-  description: 'A site to demonstrate the who and what of Zach Liibbe',
+  description: 'A site to demonstrate the who and what of Zach',
 }
 
 export default function RootLayout({
@@ -14,8 +15,23 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+    // <html lang="en">
+    //   <body className={inter.className}>{children}</body>
+    // </html>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <main>
+          <nav>
+            {/* <Link href="/">
+              Home
+            </Link>
+            <Link href="/notes">
+              Notes
+            </Link> */}
+          </nav>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
