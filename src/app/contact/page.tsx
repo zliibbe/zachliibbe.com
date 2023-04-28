30337e7f52f0155b33c08c323f7302d8f7f27019
Zach Liibbe <zliibbe@gmail.com>
2023-04-28 15:58:15 -0600

    Add Contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,15 @@
+import Layout from "../dashboard/layout";
+import styles from '../page.module.css'
+
+
+export default function Contact() {
+  return (
+    <>
+      <Layout>
+        <main className={styles.main}>
+          <h1>Contact page</h1>
+        </main>
+      </Layout>
+    </>
+  )
+}
