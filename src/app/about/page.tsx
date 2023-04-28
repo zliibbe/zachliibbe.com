20d5302b713d8f06c335981341731f8a40d0d21f
Zach Liibbe <zliibbe@gmail.com>
2023-04-28 15:57:20 -0600

    Add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import Layout from "../dashboard/layout";
+import styles from '../page.module.css'
+
+
+export default function About() {
+  return (
+    <Layout>
+      <main className={styles.main}>
+        <h1>About page</h1>
+      </main>
+    </Layout>
+  )
+}
