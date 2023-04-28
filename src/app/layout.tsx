31b136f9133c7a19c5c5219d080e51fed1277d6c
Zach Liibbe <zliibbe@gmail.com>
2023-04-28 16:00:01 -0600

    Add RootLayout per documentation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,44 +1,9 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Link from 'next/link';
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'Zach Liibbe',
-  description: 'A site to demonstrate the who and what of Zach',
-  icons: {
-    icon: {
-      url: "/favicon.png",
-      type: "image/png",
-    },
-    shortcut: { url: "/favicon.png", type: "image/png" },
-  },
-}
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+export default function RootLayout({ children }: {
+  children: React.ReactNode;
 }) {
   return (
-    // <html lang="en">
-    //   <body className={inter.className}>{children}</body>
-    // </html>
     <html lang="en">
-      <body>
-        <main>
-          <nav>
-            {/* <Link href="/">
-              Home
-            </Link>
-            <Link href="/notes">
-              Notes
-            </Link> */}
-          </nav>
-          {children}
-        </main>
-      </body>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
