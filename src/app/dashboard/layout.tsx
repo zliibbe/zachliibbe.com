75683f8f77f902dbc48a4b1dbcd23d890cedd87b
Zach Liibbe <zliibbe@gmail.com>
2023-04-28 15:58:49 -0600

    Add Dashboard page with layout

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,37 @@
+import '../globals.css';
+import styles from '../page.module.css'
+
+import { Inter } from 'next/font/google'
+import Link from 'next/link';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Zach Liibbe Portfolio',
+  description: 'A site to demonstrate the who and what of Zach',
+  icons: {
+    icon: {
+      url: "/favicon.png",
+      type: "image/png",
+    },
+    shortcut: { url: "/favicon.png", type: "image/png" },
+  },
+}
+
+export default function Layout({ children }: {
+  children: React.ReactNode;
+}) {
+  return (
+  
+    <main>
+      <Header/>
+      {children} 
+      <Footer/>
+    </main>
+  
+      
+  )
+}
