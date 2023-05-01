69419a439a6c8fe45572edd348ed25f5cb651e0b
Zach Liibbe <zliibbe@gmail.com>
2023-05-01 13:55:34 -0600

    Update menu links

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,14 +3,17 @@ import styles from '../page.module.css'
 
 const Header = () => {
   return (    
-    <nav>
-      <ul className={styles.center}>
-        <Link href="/" className={styles.description}>Home</Link>
-        <Link href="/about" className={styles.description}>About</Link>
-        <Link href="/work" className={styles.description}>Work</Link>
-        <Link href="/contact" className={styles.description}>Contact</Link>
-      </ul>
-    </nav>
+    
+    <div>
+      <nav>
+        <ul className={styles.center}>
+          <Link href="/" className={styles.description}>Home</Link>
+          <Link href="/about" className={styles.description}>About</Link>
+          <Link href="/work" className={styles.description}>Work</Link>
+          <Link href="/contact" className={styles.description}>Contact</Link>
+        </ul>
+      </nav>
+    </div>
     );
 }
  
