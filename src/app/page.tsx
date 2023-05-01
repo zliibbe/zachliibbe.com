3682917b7a075b8a4da0dc871e6b8629c6cc17bc
Zach Liibbe <zliibbe@gmail.com>
2023-05-01 13:56:33 -0600

    Remove extra nonsense text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,16 @@ export default function Home() {
   return (
     <Layout>
       <main className={styles.main}>
+        <div className={styles.flex}>
+          <h5 className={styles.description}>Hey there, I'm Zach!</h5>
+          <h1 className={styles.center}>Full Stack Engineer</h1>
+          <br></br>
+          <p className={styles.center}>simple design, powerful impact</p>
+        </div>
+        
         <h1>Heading in <code>src/app/page.tsx</code></h1>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore dolorem perferendis et eveniet rem expedita accusantium eius, voluptatum sequi blanditiis quibusdam beatae quasi enim, ullam ratione libero? Inventore, beatae amet.</p>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus sed dolore deleniti dolorum hic vero!</p>
-        <p>Cupcake ipsum dolor sit amet. Tootsie roll shortbread dragée tart fruitcake halvah chupa chups danish. Pie tart cake gummies lemon drops dragée chocolate cake powder. Cotton candy sweet dessert sweet roll jelly. Cake cupcake fruitcake sweet dessert wafer wafer pie. Pie jelly marzipan jelly sweet roll. Chocolate dragée sugar plum cake powder. Tootsie roll pudding ice cream chupa chups cotton candy pastry gummi bears muffin. Lollipop jelly beans tart tart chocolate bar donut pastry carrot cake soufflé. Jelly beans biscuit liquorice cake croissant wafer cotton candy pastry croissant. Candy canes cupcake cookie danish jelly beans wafer fruitcake. Tart gummies dessert cotton candy topping oat cake. Sweet roll fruitcake icing jelly marshmallow cookie dessert bonbon.</p>
       </main>
     </Layout>
   )
