ad295bfdfb8e58932b064060345c9a30ed54a6e4
Zach Liibbe <zliibbe@gmail.com>
2023-05-03 10:29:44 -0600

    Update basic content of work page

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -3,7 +3,18 @@ import Layout from "../dashboard/layout"
 export default  function Work() {
   return (
     <Layout>
-      <h1>I'm the work page</h1>
+      <h1>currently a work in progress...</h1>
+      <h2 className="overview">Overview</h2>
+      <p className="overview"> overview in 3-5 lines goes here</p>
+  
+      <div className="month">
+        What I'm up to in "$ {"month"}"
+        <ul>
+          <li>Something technical</li>
+          <li>Something non-techincal</li>
+          <li>Something fun</li>
+        </ul>
+      </div>
     </Layout>
   )
 }
