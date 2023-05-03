ef7370c01d70c22167c50caa2dda7673178c216d
Zach Liibbe <zliibbe@gmail.com>
2023-05-03 10:27:48 -0600

    Update Links in header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,19 +1,19 @@
 import Link from 'next/link';
 import styles from '../page.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMountainSun, faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
   return (    
-    
-    <div>
-      <nav>
-        <ul className={styles.center}>
-          <Link href="/" className={styles.description}>Home</Link>
-          <Link href="/about" className={styles.description}>About</Link>
-          <Link href="/work" className={styles.description}>Work</Link>
-          <Link href="/contact" className={styles.description}>Contact</Link>
+      <nav className={styles.flex}>
+        <ul className={styles.flex}>
+          <li><Link href="/" className={`home-icon ${styles.description}`}><FontAwesomeIcon icon={ faMountainSun }/></Link></li>
+          <li><Link href="/about" className={styles.description}>About</Link></li>
+          <li><Link href="/work" className={styles.description}>Work</Link></li>
+          <li><Link href="/contact" className={styles.description}>Contact</Link></li>
+          <FontAwesomeIcon icon={faBars}/>
         </ul>
       </nav>
-    </div>
     );
 }
  
