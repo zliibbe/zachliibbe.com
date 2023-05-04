1357f70ed68db2d17b52c70bdd89b141f1b2ccc5
Zach Liibbe <zliibbe@gmail.com>
2023-05-04 09:08:45 -0600

    Adjust size of zach image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,7 @@ export default function Home() {
           <Image 
             src={zachPic}
             alt="Zach standing" 
+            height="350"
             />
         </section>
         
