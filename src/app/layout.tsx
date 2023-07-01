581e5cb344e9572c512873e2a0d574d2eaa25f8b
Zach Liibbe <zliibbe@gmail.com>
2023-06-30 21:34:23 -0600

    Update fonts in src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,27 @@
+import { Plus_Jakarta_Sans, Roboto_Mono } from "next/font/google";
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-plus-jakarta-sans",
+});
+
+const robotoMono = Roboto_Mono({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto-mono",
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={`${plusJakartaSans.variable} ${robotoMono.variable}`}
+    >
       <body>{children}</body>
     </html>
   );
