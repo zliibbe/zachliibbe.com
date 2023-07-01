c5c540c8c0cbe250f291e1f73eb61cb959569b60
Zach Liibbe <zliibbe@gmail.com>
2023-06-30 23:42:22 -0600

    Add padding, begin CSS

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -15,46 +15,55 @@ export default function Footer() {
   return (
     <footer>
       <hr className={styles.divider}></hr>
-      <div className={`footer-grid, ${styles.grid}`}>
+      <div className={`footer-grid, grid`}>
         <div className={`footer-grid, ${styles.description}`}>
           <p className="live-feed">Live Feed</p>
-          <a
-            href="https://www.strava.com/athletes/zachliibbe"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faStrava} /> <p className="strava">Strava</p>
-          </a>
+          
+          <div className="flex">
+            <a
+              href="https://www.strava.com/athletes/zachliibbe"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={`row-auto`}
+            >
+              <FontAwesomeIcon icon={faStrava} /> <p className="strava">Strava</p>
+            </a>
+          </div>
+          
+
           <a
             href="https://open.spotify.com/user/zliibbe86?si=NsI7mNaCSYuBRLlYryIwYw"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <FontAwesomeIcon icon={faSpotify} />{" "}
+            <FontAwesomeIcon icon={faSpotify} className="p-2"/>{" "}
             <p className="strava">Spotify</p>
           </a>
+
         </div>
-        <div className="footer-social-and-byline">
-          <div className="footer-social-icon">
+        <div className={`footer-social-and-byline, flex, px-4`}>
+          <div className={`footer-social-icon, px-3`}>
             <a
               href="https://linkedin.com/in/zachliibbe"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <FontAwesomeIcon icon={faLinkedinIn} />
+              <FontAwesomeIcon icon={faLinkedinIn} className="w-7 p-2 m-2"/>
             </a>
+
             <a
               href="https://github.com/zliibbe"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <FontAwesomeIcon icon={faGithub} />
+              <FontAwesomeIcon icon={faGithub} className="w-7 p-2 m-2"/>
             </a>
+
             <a href="/contact">
-              <FontAwesomeIcon icon={faEnvelope} />
+              <FontAwesomeIcon icon={faEnvelope} className="w-7 p-2 m-2"/>
             </a>
           </div>
-          <div className="footer-copywright">
+          <div className={`footer-copywright, flex`}>
             Built using
             <strong>
               <a
@@ -62,7 +71,7 @@ export default function Footer() {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <span className={styles.logo}>
+                <span className={`${styles.logo}, px-3`}>
                   <Image
                     src="/next.svg"
                     alt="Next.js logo"
@@ -83,14 +92,14 @@ export default function Footer() {
                   <Image
                     src="/vercel.svg"
                     alt="Vercel logo"
-                    width={25}
-                    height={25}
+                    width={44}
+                    height={44}
                   />
                 </span>
               </a>
             </strong>
-            in
-            <strong> Colorado Springs, CO</strong>
+            in 
+            <strong>Colorado Springs, CO</strong>
           </div>
         </div>
       </div>
