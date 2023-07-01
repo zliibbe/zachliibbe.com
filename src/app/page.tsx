283ca8e291619589465e04d341dd536d1694d742
Zach Liibbe <zliibbe@gmail.com>
2023-06-30 23:39:03 -0600

    Rearrange WIP & begin implementing tailwind class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,21 +8,21 @@ import Link from "next/link";
 export default function Home() {
   return (
     <Layout>
-      <main className={styles.description}>
-        <section className={styles.description}>
+      <main className={`flex`}>
+        <h1>
+          <code>currently a Work In Progress...</code>
+        </h1>
+        <section className={`flex-col`}>
           <h5 className={styles.description}>{`Hey there, I'm Zach!`}</h5>
-          <h1 className={styles.description}>Full Stack Engineer</h1>
+          <h1 className={styles.description}>Full Stack Software Engineer</h1>
           <br></br>
           <p className={styles.description}>simple design, powerful impact</p>
         </section>
         <section>
           <div className="background-shape"></div>
-          <Image src={zachPic} alt="Zach standing" height="350" />
+          <Image src={zachPic} alt="Photo of Zach" height="350" />
         </section>
 
-        <h1>
-          <code>currently a Work In Progress...</code>
-        </h1>
         <p>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore
           dolorem perferendis et eveniet rem expedita accusantium eius,
