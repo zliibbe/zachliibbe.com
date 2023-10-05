8502fa01251214e58d97c7ccd21c458f9869a77c
Zach Liibbe <zliibbe@gmail.com>
2023-10-05 13:52:13 -0600

    Arrange code for better readability and make use of tw- prefix

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,4 +1,5 @@
 import styles from "../page.module.css";
+// import styles from "./Footer.module.css";
 import Image from "next/image";
 import nextSvg from "../../../public/next.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -14,87 +15,51 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 export default function Footer() {
   return (
     <footer>
-      <hr className={styles.divider}></hr>
-      <div className={`footer-grid, grid`}>
-        <div className={`footer-grid, ${styles.description}`}>
+      <hr className="hr"></hr>
+      <div>
+        <div className={`${styles.footer}`}>
           <p className="live-feed">Live Feed</p>
           
           <div className="flex">
-            <a
-              href="https://www.strava.com/athletes/zachliibbe"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={`row-auto`}
-            >
+            <a href="https://www.strava.com/athletes/zachliibbe" target="_blank" rel="noopener noreferrer">
               <FontAwesomeIcon icon={faStrava} /> <p className="strava">Strava</p>
             </a>
           </div>
-          
 
-          <a
-            href="https://open.spotify.com/user/zliibbe86?si=NsI7mNaCSYuBRLlYryIwYw"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faSpotify} className="p-2"/>{" "}
-            <p className="strava">Spotify</p>
+          <a href="https://open.spotify.com/user/zliibbe86?si=NsI7mNaCSYuBRLlYryIwYw" target="_blank" rel="noopener noreferrer" className="flex-auto">
+            <FontAwesomeIcon icon={faSpotify} className="spotify tw-p-2"/>{" "}<p className="strava">Spotify</p>
           </a>
 
         </div>
-        <div className={`footer-social-and-byline, flex, px-4`}>
-          <div className={`footer-social-icon, px-3`}>
-            <a
-              href="https://linkedin.com/in/zachliibbe"
-              target="_blank"
-              rel="noopener noreferrer"
+        <div className={`footer-social-and-byline, flex-auto, px-4`}>
+          <div className={`footer-social-icon, tw-px-3`}>
+            <a href="https://linkedin.com/in/zachliibbe" target="_blank" rel="noopener noreferrer"  className="linkedin"
             >
-              <FontAwesomeIcon icon={faLinkedinIn} className="w-7 p-2 m-2"/>
+              <FontAwesomeIcon icon={faLinkedinIn} className="linkedin tw-w-7 tw-p-2 tw-m-2"/>
             </a>
 
-            <a
-              href="https://github.com/zliibbe"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon icon={faGithub} className="w-7 p-2 m-2"/>
+            <a href="https://github.com/zliibbe" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faGithub} className="tw-w-7 tw-p-2 tw-m-2"/>
             </a>
 
             <a href="/contact">
-              <FontAwesomeIcon icon={faEnvelope} className="w-7 p-2 m-2"/>
+              <FontAwesomeIcon icon={faEnvelope} className="tw-w-7 tw-p-2 tw-m-2"/>
             </a>
           </div>
-          <div className={`footer-copywright, `}>
+          <div className={`footer-copywright`}>
             Built using
             <strong>
-              <a
-                href="https://nextjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className={`${styles.logo}, px-3`}>
-                  <Image
-                    src="/next.svg"
-                    alt="Next.js logo"
-                    height={44}
-                    width={44}
-                  />
+              <a href="https://nextjs.org" target="_blank" rel="noopener noreferrer" className={styles.logo}> 
+                <span className={`tw-px-3`}>
+                  <Image src="/next.svg" alt="Next.js logo" height={44} width={44}/>
                 </span>
               </a>
             </strong>
             and
             <strong>
-              <a
-                href="https://vercel.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
+              <a href="https://vercel.com/" target="_blank" rel="noopener noreferrer" >
                 <span className={styles.logo}>
-                  <Image
-                    src="/vercel.svg"
-                    alt="Vercel logo"
-                    width={44}
-                    height={44}
-                  />
+                  <Image src="/vercel.svg" alt="Vercel logo" width={44} height={44}/>
                 </span>
               </a>
             </strong>
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,33 +5,35 @@ import { faMountainSun, faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   return (
-    <nav className={`flex w-full`}>
-      <ul className={`flex text-cyan-300 `}>
-        <li className={`p-3`}>
-          <Link href="/" className={`home-icon ${styles.description} hover:text-cyan-700 hover:duration-300`}>
+    <nav className={`tw-flex tw-w-full tw-justify-center tw-content-center`}>
+      <div className={`tw-flex tw-items-center tw-content-start`}>
+        <Link href="/" className={`home-icon ${styles.description} tw-hover:text-cyan-700 hover:duration-300 tw-hover:backdrop:before:first-letter:text-white`}>
             <FontAwesomeIcon icon={faMountainSun} />
           </Link>
+      </div>
+      
+      <ul className={`home flex`}>
+        <li className={`tw-flex-initial tw-p-3 tw-hover:text-cyan-700 tw-hover:underline tw-hover:duration-500`}>
+          <Link href="/"></Link>
         </li>
-        <div className={`flex items-center `}>
-          <li className={`p-3 hover:text-cyan-700 hover:underline hover:duration-500`}>
-            <Link href="/about" className={styles.description}>
-              About
-            </Link>
+
+        <div className={`navbar tw-flex tw-items-center `}>
+          <li className={`tw-p-3 tw-hover:text-cyan-700 tw-hover:underline tw-hover:duration-500`}>
+            <Link href="/about" className={styles.description}>About</Link>
           </li>
-          <li className={`p-3 hover:text-cyan-700 hover:underline hover:duration-500`}>
-            <Link href="/work" className={styles.description}>
-              Work
-            </Link>
+          <li className={`tw-p-3 tw-hover:text-cyan-700 tw-hover:underline tw-hover:duration-500`}>
+            <Link href="/work" className={styles.description}>Work</Link>
           </li>
-          <li className={`p-3 hover:text-cyan-700 hover:underline hover:duration-500 `}>
-            <Link href="/contact" className={styles.description}>
-              Contact
-            </Link>
+          <li className={`tw-p-3 tw-hover:text-cyan-700 tw-hover:underline tw-hover:duration-500`}>
+            <Link href="/contact" className={styles.description}>Contact</Link>
           </li>
         </div>
-        <li  className={`p-3 hover:text-cyan-700 hover:duration-500 `}>
-          <FontAwesomeIcon icon={faBars}/>
-        </li>
+
+        <div className="prefs-menu">
+          <li  className={`p-3 hover:text-cyan-100 hover:duration-500`}>
+            <FontAwesomeIcon icon={faBars}/>
+          </li>
+        </div>
       </ul>
     </nav>
   );
