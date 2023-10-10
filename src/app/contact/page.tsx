7b83cb8b8d1f025b45ea14a22c2dc69058403430
Zach Liibbe <zliibbe@gmail.com>
2023-10-09 21:03:59 -0600

    Add contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,9 +5,28 @@ export default function Contact() {
   return (
     <>
       <Layout>
-        <main className={styles.main}>
-          <h1>Contact page</h1>
-        </main>
+        <div
+          className={`${styles.page_page} tw-flex tw-flex-col tw-bg-white tw-h-screen tw-mx-32 tw-my-0 tw-rounded-lg tw-p-8`}
+        >
+          <main
+            className={`${styles.main} tw-flex tw-content-center tw-justify-center`}
+          >
+            <div className="tw-flex-col">
+              <h1 className="tw-flex tw-justify-center tw-align-middle tw-py-4">
+                Contact page
+              </h1>
+              <p className="tw-flex tw-justify-center tw-py-3">
+                The quickest way to reach me is to send me an email:
+                zliibbe@gmail.com
+              </p>
+              <p className="tw-flex tw-justify-center tw-py-3">...</p>
+              <p className="tw-flex tw-justify-center tw-py-3">
+                Check back here soon to see more features added to this work in
+                progress...
+              </p>
+            </div>
+          </main>
+        </div>
       </Layout>
     </>
   );
