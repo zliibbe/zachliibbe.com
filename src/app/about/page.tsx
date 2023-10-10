31df3a09f714318615637a8ed9928f4a76e7aa9d
Zach Liibbe <zliibbe@gmail.com>
2023-10-09 21:02:55 -0600

    Add about page and css styling

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,9 +4,54 @@ import styles from "../page.module.css";
 export default function About() {
   return (
     <Layout>
-      <main className={styles.main}>
-        <h1>About page</h1>
-      </main>
+      <div
+        className={`${styles.page_page} tw-flex tw-flex-col tw-bg-white tw-h-screen tw-mx-32 tw-my-0 tw-rounded-lg tw-p-8`}
+      >
+        <div className="image-circle">
+          <img alt="headshot of zach"></img>
+        </div>
+        <div>
+          <p>Welcome!</p>
+          <p>
+            My name's Zach and I'm glad you've somehow ended up here at my work
+            in progress.
+          </p>
+          <p>I'm currently a software engineer at location.</p>
+          <br />
+          <div>
+            <p>
+              I like working on things that can ultimately be simmered down to
+              simple design and powerful impact.
+            </p>
+          </div>
+          <br />
+          <p>
+            And I love the complexity that font end coding and design work
+            brings.
+          </p>
+          `
+        </div>
+
+        <hr className="tw-flex tw-justify-center tw-content-center tw-align-middle tw-w-32 tw-p-6 tw-c" />
+        <p>Outside of work hours, you are most likely to find me:</p>
+        <ul>
+          <li>Example activity 1</li>
+        </ul>
+        <ul>
+          <li>Example activity 2</li>
+        </ul>
+        <ul>
+          <li>Example activity 3</li>
+        </ul>
+        <p>
+          "Everyone deserves a place on the web to call their own, and this is
+          my litte spot. I do my best to ensure my spot feels like a living,
+          breathing thing. Thanks for stopping by!"
+        </p>
+        <br />
+        <br />
+        <p className="tw-flex tw-justify-end tw-content-end">-Zach</p>
+      </div>
     </Layout>
   );
 }
