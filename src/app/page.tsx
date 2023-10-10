39b3c146e927acf8d88659d23d41be9afc8b218f
Zach Liibbe <zliibbe@gmail.com>
2023-10-09 21:07:16 -0600

    Update home hero view and add picture and shape

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,31 +8,30 @@ import Link from "next/link";
 export default function Home() {
   return (
     <Layout>
-      <main className={`flex`}>
-        <h1>
-          <code>currently a Work In Progress...</code>
-        </h1>
-        <section className={`flex-col`}>
+      <main className={`tw-flex tw-justify-evenly`}>
+        <section
+          className={`tw-flex tw-flex-col tw-justify-center tw-content-center`}
+        >
           <h5 className={styles.description}>{`Hey there, I'm Zach!`}</h5>
           <h1 className={styles.description}>Full Stack Software Engineer</h1>
           <br></br>
-          <p className={styles.description}>simple design, powerful impact</p>
-        </section>
-        <section>
-          <div className="background-shape"></div>
-          <Image src={zachPic} alt="Photo of Zach" height="350" />
+          <p className={styles.description}>I'm all about simple design and powerful impact</p>
         </section>
 
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore
-          dolorem perferendis et eveniet rem expedita accusantium eius,
-          voluptatum sequi blanditiis quibusdam beatae quasi enim, ullam ratione
-          libero? Inventore, beatae amet.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus sed
-          dolore deleniti dolorum hic vero!
-        </p>
+        <section className={`home-hero-image tw-flex-col`}>
+          <div>
+            <div className="background-shape">
+              <Image
+                src={zachPic}
+                alt="Photo of Zach"
+                width="250"
+                height="400"
+                decoding="async"
+                placeholder="empty"
+              />
+            </div>
+          </div>
+        </section>
       </main>
     </Layout>
   );
