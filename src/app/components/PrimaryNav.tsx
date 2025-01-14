442dd5a05bfd98bf1d542e8ee70827ab97bc04df
Zach Liibbe <63659270+zliibbe@users.noreply.github.com>
2025-01-14 15:12:26 -0700

    Add Primary Nav component

---
+++ src/app/components/PrimaryNav.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./Header.module.css";
+import Link from "next/link";
+
+export default function PrimaryNav() {
+  return (
+    <ul id="primaryNav" className={styles.primaryNav}>
+      <li>
+        <Link href="/about" className={styles.navLink}>
+          About
+        </Link>
+      </li>
+      <li>
+        <Link href="/work" className={styles.navLink}>
+          Work
+        </Link>
+      </li>
+      <li>
+        <Link href="/contact" className={styles.navLink}>
+          Contact
+        </Link>
+      </li>
+    </ul>
+  );
+}
