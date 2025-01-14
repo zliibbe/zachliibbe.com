98d7b6e30699be6a9f7559d69e6c4b376aeea1b1
Zach Liibbe <63659270+zliibbe@users.noreply.github.com>
2025-01-14 15:11:31 -0700

    Add styles folder for utilities and variables (may not keep)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,5 @@
 import Layout from "../dashboard/layout";
-import styles from "../page.module.css";
+import styles from "./page.module.css";
 import Image from "next/image";
 import headshot from "../../assets/headshot.png";
 import Footer from "../components/Footer";
@@ -8,12 +8,10 @@ import { Metadata } from "next";
 export default function About() {
   return (
     <Layout>
-      <div
-        className={`${styles.page_page} tw-flex tw-flex-col tw-bg-white tw-h-screen tw-mx-32 tw-my-0 tw-rounded-lg tw-p-8 tw-m-8`}
-      >
-        <div className="image-circle tw-flex tw-content-center tw-justify-center">
+      <div className={styles.container}>
+        <div className={styles.imageContainer}>
           <Image
-            className="headshot_circle"
+            className={styles.headshot}
             alt="Zach's Face"
             src={headshot}
             width="200"
@@ -21,16 +19,16 @@ export default function About() {
             placeholder="blur"
           />
         </div>
-        <div className="tw-flex tw-flex-col tw-align-middle tw-justify-center tw-ml-6">
-          <h3 className="tw-text-lg">
+        <div className={styles.contentWrapper}>
+          <h3 className={styles.title}>
             <strong>Welcome!</strong>
           </h3>
-          <p className="tw-flex tw-py-4">
+          <p className={styles.paragraph}>
             My name is Zach and I'm glad you've somehow ended up here.
           </p>
           <p>
             I'm a software engineer currently between jobs. I'm looking to join
-            a company whose mission I believe in and whose values I share.{" "}
+            a company whose mission I believe in and whose values I share.
           </p>
           <br />
           <div>
@@ -40,40 +38,48 @@ export default function About() {
             </p>
           </div>
           <br />
-          <p className="tw-pb-6">
+          <p className={styles.paragraph}>
             I love the complexity and creativity that frontend coding and design
             work brings.
           </p>
         </div>
 
-        <hr className="about_hr hr_about tw-flex-row tw-justify-center tw-content-center tw-align-middle tw-w-32 tw-p-6 tw-c" />
+        <hr className={styles.divider} />
 
-        <ul className="tw-flex tw-flex-col tw-align-middle tw-justify-center tw-ml-6">
+        <ul className={styles.listContainer}>
           Outside of work hours, you are most likely to find me:
         </ul>
         <br />
-        <li className="tw-ml-8">Out running with my dog, Panda. </li>
-        <li className="tw-ml-8">Building websites in order to learn more about my craft.</li>
-        <li  className="tw-ml-8">
+        <li className={styles.listItem}>Out running with my dog, Panda.</li>
+        <li className={styles.listItem}>
+          Building websites in order to learn more about my craft.
+        </li>
+        <li className={styles.listItem}>
           Reading science-fiction books. I'm currently enjoying book one of
           four:{" "}
           <a href="https://www.amazon.com/We-Are-Legion-Bob-Bobiverse/dp/1680680587">
             We Are Legion (We Are Bob)
-          </a> by Dennis E Taylor
-          </li>
-          <li className="tw-ml-8">Hanging out at home with my wife and two young daughters.</li>
-          <br />
-          <p className="tw-flex tw-flex-col tw-align-middle tw-justify-center tw-ml-6">
-            Everyone deserves a place on the web to call their own and this is my litte spot. I do my best to ensure my spot feels like a living, breathing thing. Thanks for stopping by!
-          </p>
+          </a>{" "}
+          by Dennis E Taylor
+        </li>
+        <li className={styles.listItem}>
+          Hanging out at home with my wife and two young daughters.
+        </li>
+        <br />
+        <p className={styles.contentWrapper}>
+          Everyone deserves a place on the web to call their own and this is my
+          litte spot. I do my best to ensure my spot feels like a living,
+          breathing thing. Thanks for stopping by!
+        </p>
         <br />
         <br />
-        <p className="tw-flex tw-justify-end tw-content-end tw-mr-4">-Zach</p>
+        <p className={styles.signature}>-Zach</p>
         <Footer />
       </div>
     </Layout>
   );
 }
- export const metadata: Metadata = {
-    title: 'About | zachliibbe.com'
- }
+
+export const metadata: Metadata = {
+  title: "About | zachliibbe.com",
+};
