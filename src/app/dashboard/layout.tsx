4ef89c8d1b34baf8900349f2360bdf21e0f12ccb
Zach Liibbe <63659270+zliibbe@users.noreply.github.com>
2025-01-16 15:35:22 -0700

    Remove unused imports

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,8 +1,6 @@
 "use client";
 import "../globals.css";
-import styles from "../page.module.css";
 
-import Link from "next/link";
 import Header from "../components/Header";
 import { usePathname } from "next/navigation";
 
