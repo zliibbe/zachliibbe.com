b2ddfc95e67f1bb836f970f8ac5232238e29f5f2
Zach Liibbe <63659270+zliibbe@users.noreply.github.com>
2025-01-31 12:19:04 -0700

    Update site title & description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,8 +25,10 @@ export default function RootLayout({
 }
 
 export const metadata = {
-  title: "Zach Liibbe",
-  description: "Zach Liibbe's own little corner of the web",
+  title:
+    "Zach Liibbe’s corner of the web—part work, part words, all curiosity.",
+  description:
+    "Zach Liibbe’s corner of the web—thinking, tinkering, and typing it all out. Part work, part words, all curiosity (with the occasional tangent).",
   icons: {
     icon: [{ url: "/favicon.ico", sizes: "any" }],
   },
