05e0585f0cb1fc82c88a038de98a25aa170a6bf5
Zach Liibbe <63659270+zliibbe@users.noreply.github.com>
2025-06-17 10:37:42 -0600

    Update next.config.js to use object

--- next.config.js
+++ next.config.js
@@ -27,7 +27,9 @@ const nextConfig = {
     return config;
   },
   experimental: {
-    serverActions: true,
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
   },
   env: {
     UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
